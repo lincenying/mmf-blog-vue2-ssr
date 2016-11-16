b172a43c3f57413d4368f6ad5869ff5bb4914898
lincenying <lincenying@qq.com>
2016-11-16 13:21:34 +0800

    no message

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,6 @@
 const port = 8080
 module.exports = {
-    api: 'http://www.mmxiaowu.com/api/?api=true',
+    api: 'http://localhost:'+ port +'/api/?api=true',
     proxy: 'localhost:3000',
     port
 }
