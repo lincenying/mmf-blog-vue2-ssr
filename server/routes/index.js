cc0389f82bff41a27488c62d80285cac8d23f4d0
lincenying <lincenying@qq.com>
2017-01-23 15:50:09 +0800

    路由修改

--- server/routes/index.js
+++ server/routes/index.js
@@ -25,162 +25,88 @@ router.post('/backend', (req, res) => {
 // ================ 后台 ================
 // ------- 文章 -------
 // 管理时, 获取文章列表
-router.get('/backend/article/list', isAdmin, (req, res) => {
-    backendArticle.getList(req, res)
-})
+router.get('/backend/article/list', isAdmin, backendArticle.getList)
 // 管理时, 获取单篇文章
-router.get('/backend/article/item', isAdmin, (req, res) => {
-    backendArticle.getItem(req, res)
-})
+router.get('/backend/article/item', isAdmin, backendArticle.getItem)
 // 管理时, 发布文章
-router.post('/backend/article/insert', isAdmin, multipartMiddleware, (req, res) => {
-    backendArticle.insert(req, res)
-})
+router.post('/backend/article/insert', isAdmin, multipartMiddleware, backendArticle.insert)
 // 管理时, 删除文章
-router.get('/backend/article/delete', isAdmin, (req, res) => {
-    backendArticle.deletes(req, res)
-})
+router.get('/backend/article/delete', isAdmin, backendArticle.deletes)
 // 管理时, 恢复文章
-router.get('/backend/article/recover', isAdmin, (req, res) => {
-    backendArticle.recover(req, res)
-})
+router.get('/backend/article/recover', isAdmin, backendArticle.recover)
 // 管理时, 编辑文章
-router.post('/backend/article/modify', isAdmin, multipartMiddleware, (req, res) => {
-    backendArticle.modify(req, res)
-})
+router.post('/backend/article/modify', isAdmin, multipartMiddleware, backendArticle.modify)
 // ------- 分类 -------
 // 管理时, 获取分类列表
-router.get('/backend/category/list', (req, res) => {
-    backendCategory.getList(req, res)
-})
+router.get('/backend/category/list', backendCategory.getList)
 // 管理时, 获取单个分类
-router.get('/backend/category/item', (req, res) => {
-    backendCategory.getItem(req, res)
-})
+router.get('/backend/category/item', backendCategory.getItem)
 // 管理时, 添加分类
-router.post('/backend/category/insert', multipartMiddleware, isAdmin, (req, res) => {
-    backendCategory.insert(req, res)
-})
+router.post('/backend/category/insert', multipartMiddleware, isAdmin, backendCategory.insert)
 // 管理时, 删除分类
-router.get('/backend/category/delete', isAdmin, (req, res) => {
-    backendCategory.deletes(req, res)
-})
+router.get('/backend/category/delete', isAdmin, backendCategory.deletes)
 // 管理时, 恢复分类
-router.get('/backend/category/recover', isAdmin, (req, res) => {
-    backendCategory.recover(req, res)
-})
+router.get('/backend/category/recover', isAdmin, backendCategory.recover)
 // 管理时, 编辑分类
-router.post('/backend/category/modify', isAdmin, multipartMiddleware, (req, res) => {
-    backendCategory.modify(req, res)
-})
+router.post('/backend/category/modify', isAdmin, multipartMiddleware, backendCategory.modify)
 // ------- 管理 -------
 // 后台登录
-router.post('/backend/admin/login', multipartMiddleware, (req, res) => {
-    backendUser.login(req, res)
-})
+router.post('/backend/admin/login', multipartMiddleware, backendUser.login)
 // 管理列表
-router.get('/backend/admin/list', isAdmin, (req, res) => {
-    backendUser.getList(req, res)
-})
+router.get('/backend/admin/list', isAdmin, backendUser.getList)
 // 获取单个管理员
-router.get('/backend/admin/item', isAdmin, (req, res) => {
-    backendUser.getItem(req, res)
-})
+router.get('/backend/admin/item', isAdmin, backendUser.getItem)
 // 编辑管理员
-router.post('/backend/admin/modify', isAdmin, multipartMiddleware, (req, res) => {
-    backendUser.modify(req, res)
-})
+router.post('/backend/admin/modify', isAdmin, multipartMiddleware, backendUser.modify)
 // 删除管理员
-router.get('/backend/admin/delete', isAdmin, (req, res) => {
-    backendUser.deletes(req, res)
-})
+router.get('/backend/admin/delete', isAdmin, backendUser.deletes)
 // 恢复管理员
-router.get('/backend/admin/recover', isAdmin, (req, res) => {
-    backendUser.recover(req, res)
-})
+router.get('/backend/admin/recover', isAdmin, backendUser.recover)
 
 // 用户列表
-router.get('/backend/user/list', isAdmin, (req, res) => {
-    frontendUser.getList(req, res)
-})
+router.get('/backend/user/list', isAdmin, frontendUser.getList)
 // 获取单个用户
-router.get('/backend/user/item', isAdmin, (req, res) => {
-    frontendUser.getItem(req, res)
-})
+router.get('/backend/user/item', isAdmin, frontendUser.getItem)
 // 编辑用户
-router.post('/backend/user/modify', isAdmin, multipartMiddleware, (req, res) => {
-    frontendUser.modify(req, res)
-})
+router.post('/backend/user/modify', isAdmin, multipartMiddleware, frontendUser.modify)
 // 删除用户
-router.get('/backend/user/delete', isAdmin, (req, res) => {
-    frontendUser.deletes(req, res)
-})
+router.get('/backend/user/delete', isAdmin, frontendUser.deletes)
 // 恢复用户
-router.get('/backend/user/recover', isAdmin, (req, res) => {
-    frontendUser.recover(req, res)
-})
+router.get('/backend/user/recover', isAdmin, frontendUser.recover)
 // ------ 评论 ------
 // 删除评论
-router.get('/frontend/comment/delete', isAdmin, (req, res) => {
-    frontendComment.deletes(req, res)
-})
+router.get('/frontend/comment/delete', isAdmin, frontendComment.deletes)
 // 恢复评论
-router.get('/frontend/comment/recover', isAdmin, (req, res) => {
-    frontendComment.recover(req, res)
-})
+router.get('/frontend/comment/recover', isAdmin, frontendComment.recover)
 // ================= 前台 =================
 // ------ 文章 ------
 // 前台浏览时, 获取文章列表
-router.get('/frontend/article/list', (req, res) => {
-    frontendArticle.getList(req, res)
-})
+router.get('/frontend/article/list', frontendArticle.getList)
 // 前台浏览时, 获取单篇文章
-router.get('/frontend/article/item', (req, res) => {
-    frontendArticle.getItem(req, res)
-})
+router.get('/frontend/article/item', frontendArticle.getItem)
 // 前台浏览时, 热门文章
-router.get('/frontend/trending', (req, res) => {
-    frontendArticle.getTrending(req, res)
-})
+router.get('/frontend/trending', frontendArticle.getTrending)
 // ------ 评论 ------
 // 发布评论
-router.post('/frontend/comment/insert', isUser, multipartMiddleware, (req, res) => {
-    frontendComment.insert(req, res)
-})
+router.post('/frontend/comment/insert', isUser, multipartMiddleware, frontendComment.insert)
 // 读取评论列表
-router.get('/frontend/comment/list', (req, res) => {
-    frontendComment.getList(req, res)
-})
+router.get('/frontend/comment/list', frontendComment.getList)
 // ------ 用户 ------
 // 前台注册
-router.post('/frontend/user/insert', multipartMiddleware, (req, res) => {
-    frontendUser.insert(req, res)
-})
+router.post('/frontend/user/insert', multipartMiddleware, frontendUser.insert)
 // 前台登录
-router.post('/frontend/user/login', multipartMiddleware, (req, res) => {
-    frontendUser.login(req, res)
-})
+router.post('/frontend/user/login', multipartMiddleware, frontendUser.login)
 // 前台账号读取
-router.get('/frontend/user/account', isUser, (req, res) => {
-    frontendUser.getItem(req, res)
-})
+router.get('/frontend/user/account', isUser, frontendUser.getItem)
 // 前台账号修改
-router.post('/frontend/user/account', isUser, multipartMiddleware, (req, res) => {
-    frontendUser.account(req, res)
-})
+router.post('/frontend/user/account', isUser, multipartMiddleware, frontendUser.account)
 // 前台密码修改
-router.post('/frontend/user/password', isUser, multipartMiddleware, (req, res) => {
-    frontendUser.password(req, res)
-})
+router.post('/frontend/user/password', isUser, multipartMiddleware, frontendUser.password)
 // ------ 喜欢 ------
 // 喜欢
-router.get('/frontend/like', isUser, (req, res) => {
-    frontendLike.like(req, res)
-})
+router.get('/frontend/like', isUser, frontendLike.like)
 // 取消喜欢
-router.get('/frontend/unlike', isUser, (req, res) => {
-    frontendLike.unlike(req, res)
-})
+router.get('/frontend/unlike', isUser, frontendLike.unlike)
 
 router.get('*', (req, res) => {
     res.json({
