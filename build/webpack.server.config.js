cc3925bf4a2915132000ad9b6ad14052e59d076a
林岑影 <lincenying@qq.com>
2018-04-26 21:53:39 +0800

    no message

--- build/webpack.server.config.js
+++ build/webpack.server.config.js
@@ -22,7 +22,12 @@ var config = merge(base, {
     node: {
         __dirname: true
     },
-    externals: Object.keys(require('../package.json').dependencies),
+    // https://webpack.js.org/configuration/externals/#externals
+    // https://github.com/liady/webpack-node-externals
+    externals: nodeExternals({
+        // do not externalize CSS files in case we need to import it from a dep
+        whitelist: [/\.css$/, /\?vue&type=style/]
+    }),
     plugins: [
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
