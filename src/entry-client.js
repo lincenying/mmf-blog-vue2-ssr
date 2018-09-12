f2092522ff914ba32d1e24b0d2670c00fa6e0944
林岑影 <lincenying@qq.com>
2018-09-12 17:02:33 +0800

    up

--- src/entry-client.js
+++ src/entry-client.js
@@ -21,12 +21,14 @@ import 'toastr/build/toastr.css'
 
 // 如果你需要前端错误信息监控, 可以到 https://sentry.io/ 注册个账号, 然后将 PUBLIC_DSN 替换下方的
 // 如果不需要可以直接删除
-Raven.config('https://cefc965cf10748aea0cc56659c2fe9ea@sentry.io/256376', {
-    includePaths: [/https?:\/\/www\.mmxiaowu\.com/]
-    // includePaths: [/https?:\/\/www\.mmxiaowu\.com/, /http:\/\/localhost:8080/]
-})
-    .addPlugin(RavenVue, Vue)
-    .install()
+if (process.env.NODE_ENV === 'production' && window.location.host === 'www.mmxiaowu.com') {
+    Raven.config('https://cefc965cf10748aea0cc56659c2fe9ea@sentry.io/256376', {
+        includePaths: [/https?:\/\/www\.mmxiaowu\.com/]
+        // includePaths: [/https?:\/\/www\.mmxiaowu\.com/, /http:\/\/localhost:8080/]
+    })
+        .addPlugin(RavenVue, Vue)
+        .install()
+}
 
 // 全局的进度条，在组件中可通过 $loading 访问
 const loading = (Vue.prototype.$loading = new Vue(ProgressBar).$mount())
